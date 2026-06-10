3af5acb37d13a3816974bcdd237bede1703c0403
RubenMero <jorgemerofranco@gmail.com>
2026-06-09 20:34:09 -0500

    feat: add in-memory storage for solicitudes de tutorias

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -3,3 +3,4 @@ package storage
 import "github.com/steveenacostapatino/tutorias-uleam-api/internal/models"
 
 var Disponibilidades []models.DisponibilidadDocente
+var SolicitudesTutoria []models.SolicitudTutoria
