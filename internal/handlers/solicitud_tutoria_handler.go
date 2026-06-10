1fc812dff4bc28a5c8126b419d4d6068e21a219f
RubenMero <jorgemerofranco@gmail.com>
2026-06-09 20:39:39 -0500

    feat: implement create solicitud tutoria endpoint

---
+++ internal/handlers/solicitud_tutoria_handler.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/steveenacostapatino/tutorias-uleam-api/internal/models"
+	"github.com/steveenacostapatino/tutorias-uleam-api/internal/storage"
+)
+
+func CreateSolicitudTutoria(w http.ResponseWriter, r *http.Request) {
+
+	var solicitud models.SolicitudTutoria
+
+	err := json.NewDecoder(r.Body).Decode(&solicitud)
+
+	if err != nil {
+		http.Error(w, "Datos inválidos", http.StatusBadRequest)
+		return
+	}
+
+	solicitud.ID = len(storage.SolicitudesTutoria) + 1
+
+	storage.SolicitudesTutoria = append(
+		storage.SolicitudesTutoria,
+		solicitud,
+	)
+
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(solicitud)
+}
